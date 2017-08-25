2bf796d5072d15cdd53747d63b82e781d3840912
vinhkq <kieuquangvinh92@gmail.com>
2017-08-25 19:09:58 +0700

    Multiple PHP version

--- provision.sh
+++ provision.sh
@@ -23,6 +23,8 @@ sudo firewall-cmd --permanent --zone=public --add-service=https
 sudo firewall-cmd --reload
 # Overwrite config
 cp -R /vagrant/config/setup/default.conf /etc/nginx/conf.d/default.conf
+nginx_conf="/etc/nginx/nginx.conf"
+sudo sed -i "s|user  nginx|user  vagrant|" $nginx_conf
 
 echo '==============================='
 echo '=========Setup MariaDB========='
@@ -43,20 +45,63 @@ echo '==============================='
 sudo rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sudo yum -y install yum-utils
 sudo yum -y update
-sudo yum-config-manager --enable remi-php71
-sudo yum --enablerepo=remi,remi-php71 -y install php-fpm php-common install php-opcache php-pecl-apcu php-cli php-pear php-pdo php-mysqlnd php-pgsql php-pecl-mongodb php-pecl-redis php-pecl-memcache php-pecl-memcached php-gd php-mbstring php-mcrypt php-xml
-sudo systemctl start php-fpm
-sudo systemctl enable php-fpm
-# Setup PHP for Nginx
-nginx_conf="/etc/nginx/nginx.conf"
-sudo sed -i "s|user  nginx|user  vagrant|" $nginx_conf
-php_fpm="/etc/php-fpm.d/www.conf"
+# sudo yum-config-manager --enable remi-php71
+
+# Setup PHP 5.5 for Nginx
+sudo yum -y install php55-php-bcmath php55-php-cli php55-php-common php55-php-fpm php55-php-gd php55-php-intl php55-php-mbstring php55-php-mcrypt php55-php-mysqlnd php55-php-opcache php55-php-pdo php55-php-pear php55-php-pecl-uploadprogress php55-php-soap php55-php-xml php55-php-xmlrpc php55-php-pecl-memcache php55-php-pecl-memcached php55-php-pgsql php55-php-pecl-mongodb php55-php-pecl-redis
+php55_php_fpm="/opt/remi/php55/root/etc/php-fpm.d/www.conf"
 sudo sed -i "s|user = apache|user = vagrant|" $php_fpm && \
 sudo sed -i "s|group = apache|group = vagrant|" $php_fpm && \
-sudo sed -i "s|listen = 127.0.0.1:9000|listen = /var/run/php-fpm/php-fpm.sock|" $php_fpm && \
+sudo sed -i "s|listen = 127.0.0.1:9000|listen = 127.0.0.1:9005|" $php_fpm && \
 sudo sed -i "s|;listen.owner = nobody|listen.owner = vagrant|" $php_fpm && \
 sudo sed -i "s|;listen.group = nobody|listen.group = vagrant|" $php_fpm
-sudo systemctl restart php-fpm
+sudo systemctl start php55-php-fpm
+sudo systemctl enable php55-php-fpm
+
+# Setup PHP 5.6 for Nginx
+sudo yum -y install php56-php-bcmath php56-php-cli php56-php-common php56-php-fpm php56-php-gd php56-php-intl php56-php-mbstring php56-php-mcrypt php56-php-mysqlnd php56-php-opcache php56-php-pdo php56-php-pear php56-php-pecl-uploadprogress php56-php-soap php56-php-xml php56-php-xmlrpc php56-php-pecl-memcache php56-php-pecl-memcached php56-php-pgsql php56-php-pecl-mongodb php56-php-pecl-redis
+php56_php_fpm="/opt/remi/php56/root/etc/php-fpm.d/www.conf"
+sudo sed -i "s|user = apache|user = vagrant|" $php_fpm && \
+sudo sed -i "s|group = apache|group = vagrant|" $php_fpm && \
+sudo sed -i "s|listen = 127.0.0.1:9000|listen = 127.0.0.1:9006|" $php_fpm && \
+sudo sed -i "s|;listen.owner = nobody|listen.owner = vagrant|" $php_fpm && \
+sudo sed -i "s|;listen.group = nobody|listen.group = vagrant|" $php_fpm
+sudo systemctl start php56-php-fpm
+sudo systemctl enable php56-php-fpm
+
+# Setup PHP 7.0 for Nginx
+sudo yum -y install php70-php-bcmath php70-php-cli php70-php-common php70-php-fpm php70-php-gd php70-php-intl php70-php-mbstring php70-php-mcrypt php70-php-mysqlnd php70-php-opcache php70-php-pdo php70-php-pear php70-php-pecl-uploadprogress php70-php-soap php70-php-xml php70-php-xmlrpc php70-php-pecl-memcache php70-php-pecl-memcached php70-php-pgsql php70-php-pecl-mongodb php70-php-pecl-redis
+php70_php_fpm="/opt/remi/php70/root/etc/php-fpm.d/www.conf"
+sudo sed -i "s|user = apache|user = vagrant|" $php_fpm && \
+sudo sed -i "s|group = apache|group = vagrant|" $php_fpm && \
+sudo sed -i "s|listen = 127.0.0.1:9000|listen = 127.0.0.1:9007|" $php_fpm && \
+sudo sed -i "s|;listen.owner = nobody|listen.owner = vagrant|" $php_fpm && \
+sudo sed -i "s|;listen.group = nobody|listen.group = vagrant|" $php_fpm
+sudo systemctl start php70-php-fpm
+sudo systemctl enable php70-php-fpm
+
+# Setup PHP 7.1 for Nginx
+sudo yum -y install php71-php-bcmath php71-php-cli php71-php-common php71-php-fpm php71-php-gd php71-php-intl php71-php-mbstring php71-php-mcrypt php71-php-mysqlnd php71-php-opcache php71-php-pdo php71-php-pear php71-php-pecl-uploadprogress php71-php-soap php71-php-xml php71-php-xmlrpc php71-php-pecl-memcache php71-php-pecl-memcached php71-php-pgsql php71-php-pecl-mongodb php71-php-pecl-redis
+php71_php_fpm="/opt/remi/php71/root/etc/php-fpm.d/www.conf"
+sudo sed -i "s|user = apache|user = vagrant|" $php_fpm && \
+sudo sed -i "s|group = apache|group = vagrant|" $php_fpm && \
+sudo sed -i "s|listen = 127.0.0.1:9000|listen = 127.0.0.1:9008|" $php_fpm && \
+sudo sed -i "s|;listen.owner = nobody|listen.owner = vagrant|" $php_fpm && \
+sudo sed -i "s|;listen.group = nobody|listen.group = vagrant|" $php_fpm
+sudo systemctl start php71-php-fpm
+sudo systemctl enable php71-php-fpm
+
+# Setup PHP 7.2 for Nginx
+sudo yum -y install php72-php-bcmath php72-php-cli php72-php-common php72-php-fpm php72-php-gd php72-php-intl php72-php-mbstring php72-php-mcrypt php72-php-mysqlnd php72-php-opcache php72-php-pdo php72-php-pear php72-php-pecl-uploadprogress php72-php-soap php72-php-xml php72-php-xmlrpc php72-php-pecl-memcache php72-php-pecl-memcached php72-php-pgsql php72-php-pecl-mongodb php72-php-pecl-redis
+php72_php_fpm="/opt/remi/php72/root/etc/php-fpm.d/www.conf"
+sudo sed -i "s|user = apache|user = vagrant|" $php_fpm && \
+sudo sed -i "s|group = apache|group = vagrant|" $php_fpm && \
+sudo sed -i "s|listen = 127.0.0.1:9000|listen = 127.0.0.1:9009|" $php_fpm && \
+sudo sed -i "s|;listen.owner = nobody|listen.owner = vagrant|" $php_fpm && \
+sudo sed -i "s|;listen.group = nobody|listen.group = vagrant|" $php_fpm
+sudo systemctl start php72-php-fpm
+sudo systemctl enable php72-php-fpm
+
 sudo systemctl restart nginx
 
 # Using NVM instead
@@ -141,11 +186,11 @@ cd /opt/
 echo '==============================='
 echo '=====Install Ruby by Rbenv====='
 echo '==============================='
-# cd /usr/local
-# sudo wget https://github.com/rbenv/rbenv/archive/master.zip
-# unzip master.zip
-# mv rbenv-master rbenv
-git clone git://github.com/sstephenson/rbenv.git /usr/local/rbenv
+cd /usr/local
+sudo wget https://github.com/rbenv/rbenv/archive/master.zip
+unzip master.zip
+mv rbenv-master rbenv
+# git clone git://github.com/sstephenson/rbenv.git /usr/local/rbenv
 echo '# rbenv setup' > /etc/profile.d/rbenv.sh
 echo 'export RBENV_ROOT=/usr/local/rbenv' >> /etc/profile.d/rbenv.sh
 echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /etc/profile.d/rbenv.sh
--- www/index.php
+++ www/index.php
@@ -1,59 +1,59 @@
 <?php
+phpinfo();
+    // /**
+    //  * Mysql Connect
+    //  */
+    // $servernameMySQL = 'localhost';
+    // $usernameMySQL = 'root';
+    // $passwordMySQL = 'root';
 
-    /**
-     * Mysql Connect
-     */
-    $servernameMySQL = 'localhost';
-    $usernameMySQL = 'root';
-    $passwordMySQL = 'root';
+    // try {
+    //     $connMySQL = new PDO("mysql:host=$servernameMySQL;dbname=mysql", $usernameMySQL, $passwordMySQL);
+    //     if (!$connMySQL) {
+    //         echo "MySQL: Connection failed!!! \n";
+    //     }
+    //     // set the PDO error mode to exception
+    //     $connMySQL->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     echo "MySQL: Connected successfully \n"; 
+    // } catch(PDOException $e) {
+    //     echo "MySQL: Connection failed: " . $e->getMessage() . "\n";
+    // }
 
-    try {
-        $connMySQL = new PDO("mysql:host=$servernameMySQL;dbname=mysql", $usernameMySQL, $passwordMySQL);
-        if (!$connMySQL) {
-            echo "MySQL: Connection failed!!! \n";
-        }
-        // set the PDO error mode to exception
-        $connMySQL->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "MySQL: Connected successfully \n"; 
-    } catch(PDOException $e) {
-        echo "MySQL: Connection failed: " . $e->getMessage() . "\n";
-    }
+    // echo "<br />";
 
-    echo "<br />";
+    // /**
+    //  * PostgreSQL Connect
+    //  */
+    // $servernamePostgres = 'localhost';
+    // $usernamePostgres = 'postgres';
+    // $passwordPostgres = 'root';
 
-    /**
-     * PostgreSQL Connect
-     */
-    $servernamePostgres = 'localhost';
-    $usernamePostgres = 'postgres';
-    $passwordPostgres = 'root';
+    // try {
+    //     $connPostgres = pg_connect("host=$servernamePostgres port=5432 dbname=postgres user=$usernamePostgres password=$passwordPostgres");
+    //     if ($connPostgres) {
+    //         echo "PostgreSQL: Connected successfully \n"; 
+    //     } else {
+    //         echo "PostgreSQL: Connection failed!!! \n";
+    //     }
+    // } catch(PDOException $e) {
+    //     echo "PostgreSQL: Connection failed: " . $e->getMessage() . "\n";
+    // }
 
-    try {
-        $connPostgres = pg_connect("host=$servernamePostgres port=5432 dbname=postgres user=$usernamePostgres password=$passwordPostgres");
-        if ($connPostgres) {
-            echo "PostgreSQL: Connected successfully \n"; 
-        } else {
-            echo "PostgreSQL: Connection failed!!! \n";
-        }
-    } catch(PDOException $e) {
-        echo "PostgreSQL: Connection failed: " . $e->getMessage() . "\n";
-    }
+    // echo "<br />";
 
-    echo "<br />";
+    // /**
+    //  * MongoDB Connect
+    //  */
+    // $servernameMongo = 'mongodb://localhost:27017';
 
-    /**
-     * MongoDB Connect
-     */
-    $servernameMongo = 'mongodb://localhost:27017';
-
-    try {
-        $connMongo = new MongoDB\Driver\Manager($servernameMongo);
-        if ($connMongo) {
-            echo "MongoDB: Connected successfully \n"; 
-        } else {
-            echo "MongoDB: Connection failed!!! \n";
-        }
-    } catch(Exception $e) {
-        echo "MongoDB: Connection failed: " . $e->getMessage() . "\n";
-    }
+    // try {
+    //     $connMongo = new MongoDB\Driver\Manager($servernameMongo);
+    //     if ($connMongo) {
+    //         echo "MongoDB: Connected successfully \n"; 
+    //     } else {
+    //         echo "MongoDB: Connection failed!!! \n";
+    //     }
+    // } catch(Exception $e) {
+    //     echo "MongoDB: Connection failed: " . $e->getMessage() . "\n";
+    // }
 ?>
